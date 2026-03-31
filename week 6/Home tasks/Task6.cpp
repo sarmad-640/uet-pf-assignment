bf28502d9fa9c46b63c5b4dc9361e1313d7a5e7e
sarmad-640 <sarmadbutt640+github@gmail.com>
2026-03-31 12:36:10 +0500

    week 6 task6 added some comments

--- week 6/Home tasks/Task6.cpp
+++ week 6/Home tasks/Task6.cpp
@@ -29,7 +29,18 @@ for(int i=0;i<ele-1;i++){       //ele-1 as the ast element is already being sort
     }
 }
 
-cout<<"Students in alphabetical order: ";
+//Selection Sort–like for each position i it scans the rest of the array
+// for(int i=0; i<ele;i++){
+//     for(int j=i;j<ele;j++){
+//         if(arr[j]<arr[i]){
+//             temp=arr[i];
+//             arr[i]=arr[j];
+//             arr[j]=temp;
+//         }
+//     }
+// }
+
+cout<<"Students in alphabetical order: \n";
 for(int i=0;i<ele;i++){
     cout<<arr[i]<<endl;
 }
